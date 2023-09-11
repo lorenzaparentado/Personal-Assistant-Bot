0fafc476b89bbdf2b23b1504d9829592561ae4dc
Lorenz Aparentado <lorenzaparentado@gmail.com>
2023-09-11 11:06:30 -0400

    events and tasks

--- main.py
+++ main.py
@@ -11,29 +11,37 @@ client = commands.Bot(command_prefix = '!', intents = discord.Intents.all())
 
 # Starts Discord bot
 @client.event
-
 async def on_ready():
     print("Bot is connected to Discord!")
     change_status.start()
 
-'''
-    now = datetime.datetime.now()
-    target = now.replace(hour= 11, minute= 36, second= 0, microsecond= 0)
+events = {}
 
-    if now > target:
-        target += datetime.timedelta(days=1)
+@client.command
+async def events(ctx):
+    await ctx.send("Please enter the event name:")
+    
+    def check(message):
+        return message.author == ctx.author and ctx.channel == message.channel
 
-    delay = (target - now).total_seconds()
-    daily_message.start(delay)
+    try:
+        event_name = await client.wait_for('message', check=check, timeout=60.0)
+        await ctx.send("Please enter the event date and time (e.g., 2023-09-15 14:30):")
+        event_datetime = await client.wait_for('message', check=check, timeout=60.0)
+        await ctx.send("Does this event repeat? (yes/no):")
+        event_repeats = await client.wait_for('message', check=check, timeout=60.0)
 
+        # Store the event in the dictionary
+        events[event_name.content] = {
+            'datetime': event_datetime.content,
+            'repeats': event_repeats.content.lower() == 'yes'
+        }
 
-@tasks.loop(hours=24)
-async def daily_message():
-    user = client.get_user(227143618085781504)
-    if user:
-        ctx = await client.get_context(user.message('!weather Philadelphia'))
-        await client.invoke(ctx)
-'''
+        await ctx.send("Event added successfully!")
+
+    except asyncio.TimeoutError:
+        await ctx.send("Event creation timed out. Please try again later.")
+
 # Changes the status of the bot over 5 seconds
 bot_status = cycle(["Try !ping", "Try !8", "Try !hello", "Try !weather"])
 @tasks.loop(seconds=5)
